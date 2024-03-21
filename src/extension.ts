b2f7633f1e38cc81d7f208f1e451cb51454f008f
Luke Vincent <luke.vincent@lukewasthefish.com>
2024-03-21 12:18:12 -0600

    python template

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,106 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import * as vscode from 'vscode';
+import { LanguageClient } from 'vscode-languageclient/node';
+import { registerLogger, traceError, traceLog, traceVerbose } from './common/log/logging';
+import {
+    checkVersion,
+    getInterpreterDetails,
+    initializePython,
+    onDidChangePythonInterpreter,
+    resolveInterpreter,
+} from './common/python';
+import { restartServer } from './common/server';
+import { checkIfConfigurationChanged, getInterpreterFromSetting } from './common/settings';
+import { loadServerDefaults } from './common/setup';
+import { getLSClientTraceLevel } from './common/utilities';
+import { createOutputChannel, onDidChangeConfiguration, registerCommand } from './common/vscodeapi';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let lsClient: LanguageClient | undefined;
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    // This is required to get server name and module. This should be
+    // the first thing that we do in this extension.
+    const serverInfo = loadServerDefaults();
+    const serverName = serverInfo.name;
+    const serverId = serverInfo.module;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('voice control started correctly');
+    // Setup logging
+    const outputChannel = createOutputChannel(serverName);
+    context.subscriptions.push(outputChannel, registerLogger(outputChannel));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello world - this is the voice control project');
-	});
-    // Create and show a new webview panel
-    const panel = vscode.window.createWebviewPanel(
-        'yourPanel', //For internals
-        'Voice Control', //Shown externally as a tab
-        vscode.ViewColumn.Two, 
-        {
-            enableScripts: true 
-        }
+    const changeLogLevel = async (c: vscode.LogLevel, g: vscode.LogLevel) => {
+        const level = getLSClientTraceLevel(c, g);
+        await lsClient?.setTrace(level);
+    };
+
+    context.subscriptions.push(
+        outputChannel.onDidChangeLogLevel(async (e) => {
+            await changeLogLevel(e, vscode.env.logLevel);
+        }),
+        vscode.env.onDidChangeLogLevel(async (e) => {
+            await changeLogLevel(outputChannel.logLevel, e);
+        }),
     );
 
-    // Set the HTML content for the webview
-    panel.webview.html = getWebviewContent();
-	context.subscriptions.push(disposable);
+    // Log Server information
+    traceLog(`Name: ${serverInfo.name}`);
+    traceLog(`Module: ${serverInfo.module}`);
+    traceVerbose(`Full Server Info: ${JSON.stringify(serverInfo)}`);
+
+    const runServer = async () => {
+        const interpreter = getInterpreterFromSetting(serverId);
+        if (interpreter && interpreter.length > 0) {
+            if (checkVersion(await resolveInterpreter(interpreter))) {
+                traceVerbose(`Using interpreter from ${serverInfo.module}.interpreter: ${interpreter.join(' ')}`);
+                lsClient = await restartServer(serverId, serverName, outputChannel, lsClient);
+            }
+            return;
+        }
+
+        const interpreterDetails = await getInterpreterDetails();
+        if (interpreterDetails.path) {
+            traceVerbose(`Using interpreter from Python extension: ${interpreterDetails.path.join(' ')}`);
+            lsClient = await restartServer(serverId, serverName, outputChannel, lsClient);
+            return;
+        }
+
+        traceError(
+            'Python interpreter missing:\r\n' +
+                '[Option 1] Select python interpreter using the ms-python.python.\r\n' +
+                `[Option 2] Set an interpreter using "${serverId}.interpreter" setting.\r\n` +
+                'Please use Python 3.8 or greater.',
+        );
+    };
+
+    context.subscriptions.push(
+        onDidChangePythonInterpreter(async () => {
+            await runServer();
+        }),
+        onDidChangeConfiguration(async (e: vscode.ConfigurationChangeEvent) => {
+            if (checkIfConfigurationChanged(e, serverId)) {
+                await runServer();
+            }
+        }),
+        registerCommand(`${serverId}.restart`, async () => {
+            await runServer();
+        }),
+    );
+
+    setImmediate(async () => {
+        const interpreter = getInterpreterFromSetting(serverId);
+        if (interpreter === undefined || interpreter.length === 0) {
+            traceLog(`Python extension loading`);
+            await initializePython(context.subscriptions);
+            traceLog(`Python extension loaded`);
+        } else {
+            await runServer();
+        }
+    });
 }
 
-function getWebviewContent() {
-    return `
-        <h1>I AM AN HTML DISPLAY WINDOW!!</h1>
-        <p>Fear me.</p>
-    `;
+export async function deactivate(): Promise<void> {
+    if (lsClient) {
+        await lsClient.stop();
+    }
 }
